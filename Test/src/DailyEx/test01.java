46c89a63bb10b61e8c616ac7034ff8a53995bb9e
gussla2206 <gussla2206@gmail.com>
2023-06-16 19:17:11 +0900

    Test 혼자연습

---
+++ Test/src/DailyEx/test01.java
@@ -0,0 +1,32 @@
+package DailyEx;
+
+public class test01 {
+	
+	public static void main(String[] args) {
+		
+		int arr[][] = {{1,2,3,4,5},{6,7,8,9,10}};
+		
+		int a = 0;
+		
+		
+		while (a < arr.length) {
+			
+			int b = 0;
+			a++;
+			
+			
+			while( b < arr.length ) {
+		
+				b++;
+		
+				
+			}System.out.println(arr[a][b]);
+		}
+			
+			
+			
+		
+		
+	
+	}
+}	
