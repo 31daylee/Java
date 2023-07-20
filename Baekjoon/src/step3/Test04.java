53d1674d5f229d167f36dc6dd746c7c30d578cd6
gussla2206 <gussla2206@gmail.com>
2023-07-20 18:23:42 +0900

    백준 3 추가

--- Baekjoon/src/step3/Test04.java
+++ Baekjoon/src/step3/Test04.java
@@ -3,7 +3,7 @@ package step3;
 import java.util.Scanner;
 
 /**
- * 날짜 : 2023/07/17
+ * 날짜 : 2023/07/20
  * 이름 : 이현정
  * 내용 : 영수증
  */
@@ -12,24 +12,24 @@ public class Test04 {
 	public static void main(String[] args) {	
 		
 		Scanner sc = new Scanner(System.in);
-		int x, n;
-		x = sc.nextInt();
-		n = sc.nextInt();
-		int[] arr = new int[n];
 		
+		int sum = sc.nextInt(); // 물견 총합 
+		int b = sc.nextInt(); // 물건 총개수
 		
-		for(int i=0; i<n; i++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			arr[i]= a * b;
+		int[] arr = new int[b]; // 물건 총개수 
+		
+	
+		for(int i=0; i<b; i++) {
+			int price = sc.nextInt();
+			int num = sc.nextInt();
 			
-		}for(int k : arr) {
-			if(k == x) {
-				System.out.println("Yes");
-			}else {
-				System.out.println("");
-			}
+			sum = sum - (price * num);
+			
 		}
+		if(sum == 0) 
+			System.out.println("Yes");
+		else
+			System.out.println("No");
+		
 	}
-
 }
--- Baekjoon/src/step3/Test05.java
+++ Baekjoon/src/step3/Test05.java
@@ -1,14 +1,35 @@
 package step3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 /**
- * 날짜 : 2023/07/05
+ * 날짜 : 2023/07/20
  * 이름 : 이현정
- * 내용 : 
+ * 내용 : 코딩은 체육과목 입니다
  */
 public class Test05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 시스템으로 받은 데이터를 문자 스트림으로 변환하는 역할 
+		
+		int a = 0;
+		
+		String str = " ";
+		
+		
+		try {
+			a = Integer.parseInt(br.readLine()); // br를 사용하여 문자열 입력 받고 readLine()을 통해 사용자가 엔터 키를 누를때까지 입력된 문자열을 읽어들인다. 그 후 문자열을 정수로 변환함 
+			for(int i=1; i<=a/4; i++) {
+				System.out.print("long"+str);
+				
+		}System.out.println("int");
+			
+		} catch (Exception e) {
+			
+		}
+		
+		
 	}
 
 }
