2c32e64e4820939ed6efa9c9197cd5862ce34972
unknown <31daylee@gmail.com>
2023-10-10 18:08:06 +0900

    업데이트 완료

--- Baekjoon/src/step3/Test11.java
+++ Baekjoon/src/step3/Test11.java
@@ -1,4 +1,9 @@
 package step3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /**
  * 날짜 : 2023/09/14
  * 이름 : 이현정
@@ -7,9 +12,33 @@ package step3;
 public class Test11 {
 
 	public static void main(String[] args) {
-
 		
-	
+		try {
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			
+			int num = Integer.parseInt(br.readLine());
+			int[] sum = new int[num];
+			
+			int[] a = new int[num];
+			int[] b = new int[num];
+			
+			for(int i=0; i<num; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				a[i] = Integer.parseInt(st.nextToken());
+				b[i] = Integer.parseInt(st.nextToken());
+				sum[i] = a[i]+b[i];
+				
+			}
+			br.close();
+			
+			for(int i=0; i<num; i++) {
+				System.out.println(sum[i]);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		
 	}
